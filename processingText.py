3e30de7f1e9954376cae7eec89f49442e843142e
AndreiReply <carabelea.andrei@gmail.com>
2018-06-26 19:14:36 +0300

    imo

--- processingText.py
+++ processingText.py
@@ -12,13 +12,13 @@ from six.moves.urllib.request import urlretrieve
 from sklearn.manifold import TSNE
 
 url = 'http://mattmahoney.net/dc/'
-vocabularyLength = 10000;
-identityMatrix = np.eye(vocabularyLength + 1);
-maxIterations = 15000;
+vocabularyLength = 2000;
+identityMatrix = np.eye(vocabularyLength);
+maxIterations = vocabularyLength * 50;
 
 #we eill use an embedding of 100 features
-numFeatures = 200;
-windowSize = 3;
+numFeatures = 100;
+windowSize = 5;
 
 
 def maybe_download(filename, expected_bytes):
@@ -66,33 +66,38 @@ def build_dataset(words):
 
 
 # given a word build return the one hot label
-def getWordLabel(word):
+# given targetWord is given as default if the word is not in dictionary
+def getWordLabel(word, targetWord=""):
     global vocabularyLength
     global identityMatrix;
     global dictionary;
-    result = 0;
     if word in dictionary:
         result =  identityMatrix[dictionary[word]];
+    elif len(targetWord) > 1:
+        assert(targetWord in dictionary);
+        result = identityMatrix[dictionary[targetWord]];
     else:
-        result = identityMatrix[vocabularyLength];
+        #default the most common word label is returned, if the word is not in dictionary
+        result = identityMatrix[0];
 
-    result.shape = (1, vocabularyLength + 1);
+    result.shape = (1, vocabularyLength);
     return result;
 
 def getLabelWord(label):
-    index = np.argmax(label)
-    if index < vocabularyLength:
-        return reverse_dictionary[index];
-    return "."
+   return reverse_dictionary(np.argmax(label))
 
 
 # the file contains a text file with phrases.
 filename = maybe_download('text8.zip', 31344016)
 words = read_data(filename)
-totalNumberOfWords = (float)(len(set(words)))
 print('Data size %d' % len(words))
 
 words = [word for word in words if len(word) > 3]
+numberOfWords = len(words);
+
+
+
+
 dictionary, reverse_dictionary = build_dataset(words)
 
 graph = tf.Graph()
@@ -105,11 +110,12 @@ def softmax(x):
 def modelCbow(weights1, biases1, weights2, biases2, train_dataset):
 
     train_dataset = train_dataset.split();
-    batch_data = np.ndarray(dtype=np.float32, shape=(0, vocabularyLength + 1));
+    batch_data = np.ndarray(dtype=np.float32, shape=(0, vocabularyLength));
     for word in train_dataset:
+        assert(word in dictionary);
         batch_data = np.append(batch_data, getWordLabel(word))
 
-    batch_data = batch_data.reshape((-1, vocabularyLength + 1))
+    batch_data = batch_data.reshape((-1, vocabularyLength))
 
     y1 = batch_data @ weights1 + biases1
     y1_reduced = np.mean(y1, 0)
@@ -117,8 +123,15 @@ def modelCbow(weights1, biases1, weights2, biases2, train_dataset):
     y2 = y1_reduced @ weights2 + biases2;
 
     #skip softmax
+    indexes = np.argsort(y2)
 
-    return getLabelWord(y2);
+    train_datasetIndex = list(map((lambda x: dictionary[x]), train_dataset))
+
+    countDown = 0
+    while indexes[0, vocabularyLength-1-countDown] in train_datasetIndex:
+        countDown+=1
+
+    return reverse_dictionary[indexes[0, vocabularyLength-1-countDown]]
 
 
 #train dataset has shape (1, vocabularyLength + 1)
@@ -132,6 +145,7 @@ def modelSkipGram(weights1, biases1, weights2, biases2, train_dataset):
 
     train_dataset = train_dataset.split();
     assert(len(train_dataset) == 1);
+    assert(train_dataset[0] in dictionary);
 
     inputLabel = getWordLabel(train_dataset[0])
 
@@ -142,8 +156,21 @@ def modelSkipGram(weights1, biases1, weights2, biases2, train_dataset):
 
     labels = [];
 
-    for i in range(2 * windowSize):
-        labels.append(getLabelWord(identityMatrix[indexes[0, vocabularyLength-i]]));
+    count = 0
+    i = -1
+    while True:
+
+        i+= 1
+        currIndex = indexes[0, vocabularyLength - 1 - i];
+
+        if dictionary[train_dataset[0]] == currIndex:
+            continue;
+
+        labels.append(reverse_dictionary[currIndex]);
+
+        count += 1
+        if count == 2 * windowSize:
+            break;
 
 
     #skip softmax
@@ -156,18 +183,18 @@ with graph.as_default():
     # Load the training, validation and test data into constants that are
     # attached to the graph.
 
-    tf_train_dataset = tf.placeholder(tf.float32, shape=(2*windowSize, vocabularyLength + 1));
-    tf_train_label = tf.placeholder(tf.float32, shape=(1, vocabularyLength + 1));
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(2*windowSize, vocabularyLength));
+    tf_train_label = tf.placeholder(tf.float32, shape=(1, vocabularyLength));
 
 
     # Variables.
     # These are the parameters that we are going to be training. The weight
     # matrix will be initialized using random values following a (truncated)
     # normal distribution. The biases get initialized to zero.
-    weights1 = tf.Variable(tf.truncated_normal([vocabularyLength + 1, numFeatures]))
-    weights2 = tf.Variable(tf.truncated_normal([numFeatures, vocabularyLength + 1]))
+    weights1 = tf.Variable(tf.truncated_normal([vocabularyLength, numFeatures]))
+    weights2 = tf.Variable(tf.truncated_normal([numFeatures, vocabularyLength]))
     biases1  = tf.Variable(tf.truncated_normal([ numFeatures]))
-    biases2  = tf.Variable((tf.truncated_normal([ vocabularyLength + 1])))
+    biases2  = tf.Variable((tf.truncated_normal([ vocabularyLength])))
 
     # Training computation.
     # We multiply the inputs with the weight matrix, and add biases. We compute
@@ -186,29 +213,45 @@ with graph.as_default():
 
 with tf.Session(graph=graph) as session:
      tf.global_variables_initializer().run();
-     for i in range(0, min(len(words), maxIterations) ,windowSize):
 
+
+     for index in range(0, min(numberOfWords, maxIterations)):
+
+         i = random.randint(0, numberOfWords - 1)
          if len(words[i]) < 4 or not words[i] in dictionary:
              continue;
 
          batch_label = getWordLabel(words[i])
-         batch_data = np.ndarray(dtype=np.float32, shape=(0, vocabularyLength + 1));
+         batch_data = np.ndarray(dtype=np.float32, shape=(0, vocabularyLength));
          for index1 in [index2 for index2 in range(i - windowSize, i + windowSize + 1) if index2 != i]:
              if index1 < len(words) and index1 >= 0:
-                 batch_data = np.append(batch_data, getWordLabel(words[index1]))
+                 batch_data = np.append(batch_data, getWordLabel(words[index1], words[i]))
              else:
                  batch_data = np.append(batch_data, batch_label)
 
-         batch_data = batch_data.reshape((2 * windowSize ,vocabularyLength + 1))
+         batch_data = batch_data.reshape((2 * windowSize ,vocabularyLength));
          fd = {tf_train_dataset: batch_data, tf_train_label: batch_label}
          _, returnedLoss, predictions = session.run([optimizer, loss, tf_train_prediction], feed_dict=fd);
-         if i%50 == 0:
-            print("iteration " + str(i) + "/" + str(len(words)) + ", loss: " + str(returnedLoss));
+         if index % 50 == 0:
+            print("iteration " + str(index) + "/" + str(maxIterations) + ", loss: " + str(returnedLoss));
 
      # if the results are not returned by session.run, they can be evalua
-     print(modelCbow(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "mountain snow ski forest"))
-     print(modelCbow(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "painter  artist musician poet"))
-     print(modelCbow(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "king ruler lord boss"))
+
+     maxTestWord = 0;
+     while True:
+
+         i = random.randint(0, numberOfWords - 1)
+         if len(words[i]) < 4 or not words[i] in dictionary:
+             continue;
+         maxTestWord += 1
+
+         print(words[i] + "[cbow]" + modelCbow(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), words[i]))
+
+         if maxTestWord > 10:
+             break
+
+     # print(modelCbow(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "painter  artist musician poet"))
+     # print(modelCbow(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "king ruler lord boss"))
 
 
 #This is skip-gram
@@ -217,18 +260,18 @@ with graph.as_default():
     # Load the training, validation and test data into constants that are
     # attached to the graph.
 
-    tf_train_dataset = tf.placeholder(tf.float32, shape=(1, vocabularyLength + 1));
-    tf_train_labels = tf.placeholder(tf.float32, shape=(2 * windowSize, vocabularyLength + 1));
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(1, vocabularyLength));
+    tf_train_labels = tf.placeholder(tf.float32, shape=(2 * windowSize, vocabularyLength));
 
 
     # Variables.
     # These are the parameters that we are going to be training. The weight
     # matrix will be initialized using random values following a (truncated)
     # normal distribution. The biases get initialized to zero.
-    weights1 = tf.Variable(tf.truncated_normal([vocabularyLength + 1, numFeatures]))
-    weights2 = tf.Variable(tf.truncated_normal([numFeatures, vocabularyLength + 1]))
+    weights1 = tf.Variable(tf.truncated_normal([vocabularyLength, numFeatures]))
+    weights2 = tf.Variable(tf.truncated_normal([numFeatures, vocabularyLength]))
     biases1  = tf.Variable(tf.truncated_normal([ numFeatures]))
-    biases2  = tf.Variable((tf.truncated_normal([ vocabularyLength + 1])))
+    biases2  = tf.Variable((tf.truncated_normal([ vocabularyLength])))
 
     # Training computation.
     # We multiply the inputs with the weight matrix, and add biases. We compute
@@ -244,27 +287,41 @@ with graph.as_default():
     tf_train_prediction = tf.nn.softmax(y2);
 
 with tf.Session(graph=graph) as session:
-     tf.global_variables_initializer().run();
-     for i in range(0, min(len(words), maxIterations) ,windowSize):
 
+     tf.global_variables_initializer().run();
+     for index in range(0, min(numberOfWords, maxIterations)):
+
+         i = random.randint(0, numberOfWords - 1)
          if len(words[i]) < 4 or not words[i] in dictionary:
              continue;
 
          batch_data = getWordLabel(words[i])
-         batch_labels = np.ndarray(dtype=np.float32, shape=(0, vocabularyLength + 1));
+         batch_labels = np.ndarray(dtype=np.float32, shape=(0, vocabularyLength));
          for index1 in [index2 for index2 in range(i - windowSize, i + windowSize + 1) if index2 != i]:
              if index1 < len(words) and index1 >= 0:
-                 batch_labels = np.append(batch_labels, getWordLabel(words[index1]))
+                 batch_labels = np.append(batch_labels, getWordLabel(words[index1], words[i]))
              else:
                  batch_labels = np.append(batch_labels, batch_data)
 
-         batch_labels = batch_labels.reshape((2 * windowSize ,vocabularyLength + 1))
+         batch_labels = batch_labels.reshape((2 * windowSize ,vocabularyLength))
          fd = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
          _, returnedLoss, predictions = session.run([optimizer, loss, tf_train_prediction], feed_dict=fd);
-         if i % 50 == 0:
-            print("iteration " + str(i) + "/" + str(len(words)) + ", loss: " + str(returnedLoss));
+         if index % 50 == 0:
+            print("iteration " + str(index) + "/" + str(maxIterations) + ", loss: " + str(returnedLoss));
 
-     # if the results are not returned by session.run, they can be evalua
-     print(modelSkipGram(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "snow"))
-     print(modelSkipGram(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "artist"))
-     print(modelSkipGram(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "fruit"))
+     # # if the results are not returned by session.run, they can be evalua
+     # print(modelSkipGram(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "snow"))
+     # print(modelSkipGram(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "artist"))
+     # print(modelSkipGram(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), "fruit"))
+
+     maxTestWord = 0;
+     while True:
+
+         i = random.randint(0, numberOfWords - 1)
+         if len(words[i]) < 4 or not words[i] in dictionary:
+             continue;
+         maxTestWord += 1
+         print(words[i] + "[skipgram]" + str(modelSkipGram(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), words[i])))
+
+         if maxTestWord > 10:
+             break
