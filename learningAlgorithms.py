e2557fca81ebee736db663044400c4560b90e2e6
Andrei Carabelea <carabelea.andrei@gmail.com>
2018-12-08 00:09:26 +0200

    as

--- learningAlgorithms.py
+++ learningAlgorithms.py
@@ -3,6 +3,7 @@ import tensorflow as tf;
 import random
 
 from sklearn.metrics.pairwise import cosine_similarity
+from math import inf
 
 vocabularyLength = 100;
 identityMatrix = np.eye(vocabularyLength);
@@ -49,156 +50,91 @@ def getNsimilarWords(word, weights1, biases1, dictionary, N):
 
 
 
-def trainCbow(graph, words, dictionary):
+
+
+def trainCbow(graph, words, dictionary, batchSize):
     #This is cbow
-    numberOfWords = len(words)
+    val = list(map(lambda x : getWordLabel(x, dictionary), words))
+    train_dataset = np.array(val)
+    train_dataset.shape = (-1, vocabularyLength)
+    
+    
+    train_labels = np.array(train_dataset)
+    
+    
+    for i in range(len(words)):
+        train_labels[i] = train_dataset[max(i - windowSize, 0)] + train_dataset[min(i + windowSize, vocabularyLength -1 )]
+        
+    
+    
+    offsetValidation =  (int) ( 0.7 * len(words))
+    
+    temp = np.copy(train_dataset)
+    train_dataset = temp[:offsetValidation, :]
+    train_labels = temp[:offsetValidation, :]
+    
+    valid_dataset = temp[offsetValidation:, :]
+    valid_labels = temp[offsetValidation:, :]
+    
+    
     with graph.as_default():
 
 
-        tf_train_dataset = tf.placeholder(tf.float32, shape=(2*windowSize, vocabularyLength));
-        tf_train_label = tf.placeholder(tf.float32, shape=(1, vocabularyLength));
+        tf_train_dataset = tf.placeholder(dtype = tf.float32, shape=(batchSize, vocabularyLength));
+        tf_train_label = tf.placeholder(dtype = tf.float32, shape=(batchSize, vocabularyLength));
+        
+        tf_valid_dataset = tf.constant(valid_dataset, dtype = tf.float32)
+        tf_valid_label =  tf.constant(valid_labels, dtype = tf.float32)
+        
 
-        weights1 = tf.Variable(tf.truncated_normal([vocabularyLength, numFeatures]))
-        weights2 = tf.Variable(tf.truncated_normal([numFeatures, vocabularyLength]))
-        biases1  = tf.Variable(tf.truncated_normal([ numFeatures]))
-        biases2  = tf.Variable((tf.truncated_normal([ vocabularyLength])))
+        weights1 = tf.Variable(tf.truncated_normal([vocabularyLength, numFeatures]), dtype = tf.float32)
+        weights2 = tf.Variable(tf.truncated_normal([numFeatures, vocabularyLength]), dtype = tf.float32)
+        biases1  = tf.Variable(tf.truncated_normal([ numFeatures]), dtype = tf.float32)
+        biases2  = tf.Variable((tf.truncated_normal([ vocabularyLength])), dtype = tf.float32)
 
 
         y1 = tf.matmul(tf_train_dataset, weights1) + biases1
-        y1_reduced = tf.reduce_mean(y1, 0);
-        y1_reduced = tf.reshape(y1_reduced, shape = (1, y1_reduced.shape[0]))
-
-        y2 = tf.matmul(y1_reduced, weights2) + biases2;
+        y1 = tf.nn.leaky_relu(y1)
+        y2 = tf.matmul(y1, weights2) + biases2;
 
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_label, logits=y2));
         optimizer = tf.train.AdamOptimizer(0.01).minimize(loss);
-        tf_train_prediction = tf.nn.softmax(y2);
+        
+        
+        y1v = tf.matmul(tf_valid_dataset, weights1) + biases1
+        y1v = tf.nn.leaky_relu(y1v)     
+        y2v = tf.matmul(y1v, weights2) + biases2;
+        
+        lossV = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_valid_label, logits=y2v));
+        
+        
+        
 
     with tf.Session(graph=graph) as session:
-         tf.global_variables_initializer().run();
+        tf.global_variables_initializer().run();
+        index = 0
+        lastVError = inf
+        
+        while True:
+            offset = index * batchSize
+            if (offset +  batchSize) > offsetValidation:
+                offset = 0
+                 
+            batch_label = train_labels[offset:offset + batchSize]
+            batch_data  = train_dataset[offset:offset + batchSize]
+         
+            fd = {tf_train_dataset: batch_data, tf_train_label: batch_label}
+            _, returnedLoss, lossValidation = session.run([optimizer, loss, lossV], feed_dict=fd);
+            if index % 5 == 0:
+                print("iteration " + str(index) + "/" + str(maxIterations) + ", loss: " + str(returnedLoss) + ", validation  loss " + str(lossValidation))
+                if lossValidation > lastVError:
+                    break
+                lastVError = lossValidation
+                
+            index+= 1
+
+        return weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval()
 
 
-         for index in range(0,  maxIterations):
-
-             i = random.randint(0, numberOfWords - 1)
-
-             batch_label = getWordLabel(words[i], dictionary)
-             batch_data = np.ndarray(dtype=np.float32, shape=(0, vocabularyLength));
-             for index1 in [index2 for index2 in range(i - windowSize, i + windowSize + 1) if index2 != i]:
-                 if index1 < len(words) and index1 >= 0:
-                     batch_data = np.append(batch_data, getWordLabel(words[index1], dictionary))
-                 else:
-                     batch_data = np.append(batch_data, batch_label)
-
-             batch_data = batch_data.reshape((2 * windowSize ,vocabularyLength));
-             fd = {tf_train_dataset: batch_data, tf_train_label: batch_label}
-             _, returnedLoss, predictions = session.run([optimizer, loss, tf_train_prediction], feed_dict=fd);
-             if index % 50 == 0:
-                print("iteration " + str(index) + "/" + str(maxIterations) + ", loss: " + str(returnedLoss));
-
-         return weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval()
-
-def trainSkipGram(graph, words, dictionary):
-
-    numberOfWords = len(words)
-    #This is skip-gram
-    with graph.as_default():
-
-        tf_train_dataset = tf.placeholder(tf.float32, shape=(1, vocabularyLength));
-        tf_train_labels = tf.placeholder(tf.float32, shape=(2 * windowSize, vocabularyLength));
-
-        weights1 = tf.Variable(tf.truncated_normal([vocabularyLength, numFeatures]))
-        weights2 = tf.Variable(tf.truncated_normal([numFeatures, vocabularyLength]))
-        biases1  = tf.Variable(tf.truncated_normal([ numFeatures]))
-        biases2  = tf.Variable((tf.truncated_normal([ vocabularyLength])))
 
 
-        y1 = tf.matmul(tf_train_dataset, weights1) + biases1
-        y2 = tf.matmul(y1, weights2) + biases2;
-        y2rep = tf.ones([2 * windowSize, 1]) *  y2;
-
-        # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=y2rep));
-        loss = tf.reduce_mean((y2rep - tf_train_labels)**2);
-        optimizer = tf.train.AdamOptimizer(0.01).minimize(loss);
-        tf_train_prediction = tf.nn.softmax(y2);
-
-    with tf.Session(graph=graph) as session:
-
-         tf.global_variables_initializer().run();
-         for index in range(0, maxIterations):
-
-             i = random.randint(0, numberOfWords - 1)
-
-             batch_data = getWordLabel(words[i], dictionary)
-             batch_labels = np.ndarray(dtype=np.float32, shape=(0, vocabularyLength));
-             for index1 in [index2 for index2 in range(i - windowSize, i + windowSize + 1) if index2 != i]:
-                 if index1 < len(words) and index1 >= 0:
-                     batch_labels = np.append(batch_labels, getWordLabel(words[index1], dictionary))
-                 else:
-                     batch_labels = np.append(batch_labels, batch_data)
-
-             batch_labels = batch_labels.reshape((2 * windowSize ,vocabularyLength))
-             fd = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
-             _, returnedLoss, predictions = session.run([optimizer, loss, tf_train_prediction], feed_dict=fd);
-             if index % 50 == 0:
-                print("iteration " + str(index) + "/" + str(maxIterations) + ", loss: " + str(returnedLoss));
-         return   weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval()
-
-#these functions use the output of the network to deduct the most similar words, not quite correct
-def modelCbow(weights1, biases1, weights2, biases2, train_dataset, dictionary, reverse_dictionary):
-
-    train_dataset = train_dataset.split();
-    batch_data = np.ndarray(dtype=np.float32, shape=(0, vocabularyLength));
-    for word in train_dataset:
-        assert(word in dictionary);
-        batch_data = np.append(batch_data, getWordLabel(word, dictionary))
-
-    batch_data = batch_data.reshape((-1, vocabularyLength))
-
-    y1 = batch_data @ weights1 + biases1
-    y1_reduced = np.mean(y1, 0)
-    y1_reduced = np.reshape(y1_reduced, (1, y1_reduced.shape[0]))
-    y2 = y1_reduced @ weights2 + biases2;
-
-    #skip softmax
-    indexes = np.argsort(y2)
-
-    train_datasetIndex = list(map((lambda x: dictionary[x]), train_dataset))
-
-    countDown = 0
-    while indexes[0, vocabularyLength-1-countDown] in train_datasetIndex:
-        countDown+=1
-    return reverse_dictionary[indexes[0, vocabularyLength-1-countDown]]
-
-
-def modelSkipGram(weights1, biases1, weights2, biases2, train_dataset, dictionary, reverse_dictionary):
-
-    train_dataset = train_dataset.split();
-    assert(len(train_dataset) == 1);
-    assert(train_dataset[0] in dictionary);
-
-    inputLabel = getWordLabel(train_dataset[0], dictionary)
-
-    y1 = inputLabel @ weights1 + biases1
-    y2 = y1 @ weights2 + biases2;
-
-    indexes = np.argsort(y2)
-
-    labels = [];
-
-    count = 0
-    i = -1
-    while True:
-
-        i+= 1
-        currIndex = indexes[0, vocabularyLength - 1 - i];
-
-        if dictionary[train_dataset[0]] == currIndex:
-            continue;
-
-        labels.append(reverse_dictionary[currIndex]);
-
-        count += 1
-        if count == 2 * windowSize:
-            break;
-    return labels
--- processingText.py
+++ processingText.py
@@ -61,8 +61,10 @@ def build_dataset(words):
 #    return reverse_dictionary(np.argmax(label))
 
 # the file contains a text file with phrases.
-filename = maybe_download('text8.zip', 31344016)
+# filename = maybe_download('text8.zip', 31344016)
 # words = read_data(filename)
+
+print("Hellosdsdssd")
 words = open('data.txt', 'r').read().split();
 
 print('Data size %d' % len(words))
@@ -77,40 +79,10 @@ numberOfWords = len(words)
 graph = tf.Graph()
 
 
-weights1, biases1, _, _ = learningAlgorithms.trainCbow(graph, words, dictionary)
+weights1, biases1, _, _ = learningAlgorithms.trainCbow(graph, words, dictionary, 20)
 
 for i in range(maxSamples):
     i = random.randint(0, numberOfWords - 1)
     print(learningAlgorithms.getNsimilarWords(words[i], weights1, biases1, dictionary, 10))
 
 
-# maxTestWord = 0;
-# while True:
-#     i = random.randint(0, numberOfWords - 1)
-#     if len(words[i]) < 4 or not words[i] in dictionary:
-#         continue;
-#     maxTestWord += 1
-#
-#     print(words[i] + "[cbow]" + learningAlgorithms.modelCbow(weights1, biases1, weights2, biases2, words[i],dictionary, reverse_dictionary))
-#
-#     if maxTestWord > 10:
-#         break
-
-
-weights1, biases1, _, _ = learningAlgorithms.trainSkipGram(graph,words,dictionary)
-
-for i in range(maxSamples):
-    i = random.randint(0, numberOfWords - 1)
-    print(learningAlgorithms.getNsimilarWords(words[i], weights1, biases1, dictionary, 10))
-
-# maxTestWord = 0;
-# while True:
-#
-#     i = random.randint(0, numberOfWords - 1)
-#     if len(words[i]) < 4 or not words[i] in dictionary:
-#         continue;
-#     maxTestWord += 1
-#     print(words[i] + "[skipgram]" + str(learningAlgorithms.modelSkipGram(weights1.eval(), biases1.eval(), weights2.eval(), biases2.eval(), words[i], dictionary, reverse_dictionary)))
-#
-#     if maxTestWord > 10:
-#         break
